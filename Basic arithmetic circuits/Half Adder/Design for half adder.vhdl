6b1069f0ef94cd6afc9994846fdc0f87b7f2750f
Smarika Tripathi <134390967+Smarikatripathi@users.noreply.github.com>
2025-01-13 20:19:24 +0545

    Create Design for half adder.vhdl

---
+++ Basic arithmetic circuits/Half Adder/Design for half adder.vhdl
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity HalfAdder is
+    port (
+        A : in STD_LOGIC;
+        B : in STD_LOGIC;
+        SUM : out STD_LOGIC;
+        CARRY : out STD_LOGIC
+    );
+end HalfAdder;
+
+architecture Behavioral of HalfAdder is
+begin
+    SUM <= A xor B;
+    CARRY <= A and B;
+end Behavioral;
